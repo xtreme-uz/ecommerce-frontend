cdf9ecb6ca45422a21a21a048bfaca4fec56e25d
rustambekov <rustambekov.mail.box@gmail.com>
2021-01-20 19:58:53 +0500

    product edit IP

--- src/app/admin/components/edit-product/edit-product.component.ts
+++ src/app/admin/components/edit-product/edit-product.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ProductService } from 'src/app/services/product.service';
+
+import { ActivatedRoute, Router } from '@angular/router';
+import { Product } from '../../interfaces/Product';
 
 @Component({
   selector: 'app-edit-product',
@@ -7,9 +12,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditProductComponent implements OnInit {
 
-  constructor() { }
+  public currentProduct!: Product;
+
+  constructor(private productService: ProductService, private router:Router, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.getProduct(this.route.snapshot.params.id);
   }
 
+  private getProduct(id: number): void {
+    this.productService.getProduct(id).subscribe(
+      (res: any) => this.currentProduct = res, 
+      (err: any) => console.log(err)
+    );
+  }
+
+  public editProduct(form: NgForm): any {
+    this.productService.updateProduct(form.value).subscribe(
+      (res: any) => {
+        console.log(res);
+        this.router.navigate(['product/list']);
+      },
+      (err: any) => {
+        console.log(err);
+      }
+    )
+  }
 }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CreateProductComponent } from './admin/components/create-product/create-product.component';
+import { EditProductComponent } from './admin/components/edit-product/edit-product.component';
 import { ListProductComponent } from './admin/components/list-product/list-product.component';
 
 
 const routes: Routes = [
   {path: '', redirectTo: '/product/list', pathMatch: 'full'},
   {path: 'product/list', component: ListProductComponent},
-  {path: 'product/create', component: ListProductComponent},
-  {path: 'product/edit', component: ListProductComponent}
+  {path: 'product/create', component: CreateProductComponent},
+  {path: 'product/edit', component: EditProductComponent}
 ];
 
 @NgModule({
